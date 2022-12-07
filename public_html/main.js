abd6c8c20e494267354fe5ee0effc2abaf66d76d
ThalenAbadia <tabadia7574@student.pps.net>
2022-12-07 13:22:34 -0800

    Update main.js

--- public_html/main.js
+++ public_html/main.js
@@ -48,7 +48,6 @@ window.onload = function() {
           new typing(elements[i], JSON.parse(toRotate), period);
         }
     }
-    // INJECT CSS
     var css = document.createElement("style");
     css.type = "text/css";
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #D1D5DB}";
