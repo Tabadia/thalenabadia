eddc0b0dcc50f20c45db68248a30ed89867bc754
Thalen <tabadia7574@student.pps.net>
2023-01-09 22:52:41 -0800

    potentially final update

--- public_html/main.js
+++ public_html/main.js
@@ -74,3 +74,21 @@ $("section[id^='contact']:visible").mouseover(function () {
     $("a[id^='projectsNav'").removeClass("underline");
     $("a[id^='contactNav'").addClass("underline");
 });
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 50;
+        if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+        } else {
+        reveals[i].classList.remove("active");
+        }
+    }
+}
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
